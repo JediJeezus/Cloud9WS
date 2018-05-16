b19b087e8707735c0206302c652564c1efdc6708
JediJeezus <corrupt.animal@gmail.com>
2018-05-16 13:53:58 -0700

    add everything

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// 
+
+$('#fromSJ').click(function(event) {
+  event.preventDefault();
+    $('#fromInput').val(innerHTML='');
+    $('#fromInput').val($('#fromInput').val() + $('#fromSJ').val());
+  });
+
+$('#fromGL').click(function(event) {
+  event.preventDefault();
+    $('#fromInput').val(innerHTML='');
+    $('#fromInput').val($('#fromInput').val() + $('#fromGL').val());
+  });
+
+$('#fromFR').click(function(event) {
+  event.preventDefault();
+    $('#fromInput').val(innerHTML='');
+    $('#fromInput').val($('#fromInput').val() + $('#fromFR').val());
+  });
+
+$('#fromTL').click(function(event) {
+  event.preventDefault();
+    $('#fromInput').val(innerHTML='');
+    $('#fromInput').val($('#fromInput').val() + $('#fromTL').val());
+  });
+
+$('#fromBK').click(function(event) {
+  event.preventDefault();
+    $('#fromInput').val(innerHTML='');
+    $('#fromInput').val($('#fromInput').val() + $('#fromBK').val());
+  });
